8f6ad6cd983f41c21d5631b3425d51f831dd3611
Jhumbl <jackhumble121@gmail.com>
2018-11-25 15:13:41 +0000

    changed server code

--- server.R
+++ server.R
@@ -22,28 +22,35 @@ shinyServer(function(input, output, session){
                 plot1 <- cleanData %>%
                         select_("Country", genre, "Continent") 
                 colnames(plot1) <- c("Country", "Value", "Continent")
-                arrange(plot1, desc(Value))
+                
                 
         
                 
                 # Plot data
-                plot1 <- plot1 %>%
+               
+                plot1 %>%
+                        # arrange(desc(Value)) %>%
                         dplyr::filter(Continent %in% continent) %>%
                         mutate(Country = forcats::fct_reorder(Country, Value)) %>%
                         arrange(desc(Value)) %>%
                         head(noOfCountries) %>%
                         ggplot(aes(Country, Value, fill = Continent)) +
                         geom_bar(stat = "identity") +
-                        # ggtitle(paste("Number of films per Country: ", genre)) +
+                        scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+                        ggtitle(paste("Number of films per Country: ", genre)) +
+                        ylab("Number of Films") +
                         coord_flip() +
+                        theme_light() +
+                        labs(caption = "Data Source: 7CUSMTSD") +
+                        theme(legend.title = element_blank()) +
                         scale_fill_manual(values = c("North America" = "#3B9AB2",
                                              "South America" = "#78B7C5", 
                                              "Europe" = "#EBCC2A", 
                                              "Asia" = "#E1AF00", 
                                              "Oceania" = "#F21A00",
                                              "Africa" = "#000000"))
-                
-                print(ggplotly(plot1))
+                # plot1
+                # print(ggplotly(plot1))
       
         })
         
--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ ui <- fluidPage(
                         selectInput("Continent", "Select a Continent:",
                                     choices = c("World" ,"North America", "South America", "Africa", "Europe",
                                                 "Asia", "Oceania"), selected = "World"),
-                        sliderInput("Number of Countries", "Select the Number of Countries to be shown:",
+                        sliderInput("Number of Countries", "Select the Number of Countries to be shown for bar chart:",
                                     min = 3, max = 30, value = 20)
                 ),
                 
