3c76ee4d7260b9d89470c71d1f1369d1f18f59d7
Jhumbl <jackhumble121@gmail.com>
2018-11-25 21:56:56 +0000

    added map and dashboard

--- server.R
+++ server.R
@@ -1,8 +1,13 @@
 library(dplyr)
 library(ggplot2)
+devtools::install_github("ropensci/plotly")
 library(plotly)
 library(shiny)
+library(scales)
+library(readr)
 cleanData <- read.csv("cleanData.csv", header = TRUE)
+cleaned <- read_csv("cleanCountryData.csv", col_names = TRUE)
+cleaned <- cleaned[,-1]
 
 shinyServer(function(input, output, session){
         output$barPlot <- renderPlotly({
@@ -36,7 +41,7 @@ shinyServer(function(input, output, session){
                         head(noOfCountries) %>%
                         ggplot(aes(Country, Value, fill = Continent)) +
                         geom_bar(stat = "identity") +
-                        scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+                        scale_y_continuous(breaks = scales::pretty_breaks(n = 7)) +
                         ggtitle(paste("Number of films per Country: ", genre)) +
                         ylab("Number of Films") +
                         coord_flip() +
@@ -52,6 +57,69 @@ shinyServer(function(input, output, session){
                 # plot1
                 # print(ggplotly(plot1))
       
+        })
+        output$mapPlot <- renderPlotly({
+                # Filter by continent
+                
+                genre <- input$Genre
+                continent <- input$Continent
+                
+                if (continent == "World"){
+                        continent <- c("North America", "South America", "Africa", "Europe",
+                                       "Asia", "Oceania", "Antarctica")
+                }
+                filteredData <- dplyr::filter(cleaned, Continent %in% continent) %>%
+                        select_("long", "lat", "group", "piece", "Country", genre, "Continent")
+                
+                colnames(filteredData) <- c("long", "lat", "piece", "group", "Country", "Value", "Continent")
+                
+                filteredData$Count <- filteredData$Value - 1
+                # Number of Breaks
+                # base_breaks <- function(n = 10){
+                #   function(x) {
+                #       axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+                #   }
+                # }
+                
+                # Breaks
+                if (max(filteredData$Value) > 50000){
+                        br <- trans_breaks("log10", function(x) 10 ^ x)(c(1, 100000))
+                        lim = c(1, 100000)
+                } else if (max(filteredData$Value) > 10000){
+                        br <- trans_breaks("log10", function(x) 5 ^ x)(c(1, 200000))
+                        lim = c(1, 20000)
+                } else if (max(filteredData$Value) > 5000){
+                        br <- trans_breaks("log10", function(x) 10 ^ x)(c(1, 10000))
+                        lim = c(1, 10000)
+                } else if (max(filteredData$Value) > 1000){
+                        br <- trans_breaks("log10",n = 5 , function(x) 5 ^ x)(c(1, 50000))
+                        lim = c(1, 10000)
+                } else if (max(filteredData$Value) <= 1000){
+                        br <- trans_breaks("log10", function(x) 5 ^ x)(c(1, 10000))
+                        lim = c(1, 1000)
+                }
+                
+                
+                
+                # br <- trans_breaks("log10", function(x) 10 ^ x)(c(1, max(filteredData$Total)))
+                lb <- br
+                lb[1] <- 0
+                
+                # Plot Data, before this make sure to write final data out.
+                p <- filteredData %>% 
+                        ggplot() +
+                        geom_polygon(aes(x = long, y = lat, fill = Value, country = Country, continent = Continent, Total = Count, piece = piece),
+                                     size = 0.03, alpha = 1, color="#000000") +
+                        theme_void() +
+                        scale_fill_continuous(low = "#fcfbfd", high = "#3f007d", trans = log_trans(), limits = lim, breaks = br, labels = prettyNum(lb, big.mark = ",", scientific = FALSE)) + 
+                        coord_map(projection = "mercator")
+                # Convert ggplot to plotly
+                
+                ggplotly(p, tooltip = c("country", "continent", "Total"))
+                
+                # #edf8b1 #2c7fb8 baige to purples
+                # #e0ecf4 #8856a7 purples , limits = c(1, 100000)
+                
         })
         
         
--- ui.R
+++ ui.R
@@ -3,25 +3,76 @@ cleanData <- cleanData[, c(-2, -1,-32)]
 cleanData <- cleanData[, c(29, 1:28)]
 library(shiny)
 library(plotly)
+library(shinydashboard)
 
-ui <- fluidPage(
-        
-        titlePanel("Movies"),
-        
-        sidebarLayout(
-                
-                sidebarPanel(
-                        selectInput("Genre", "Select a Genre:",  
-                                    choices = c(names(cleanData)), selected = "Total"),
-                        selectInput("Continent", "Select a Continent:",
-                                    choices = c("World" ,"North America", "South America", "Africa", "Europe",
-                                                "Asia", "Oceania"), selected = "World"),
-                        sliderInput("Number of Countries", "Select the Number of Countries to be shown for bar chart:",
-                                    min = 3, max = 30, value = 20)
+ui <- dashboardPage(
+        dashboardHeader(title = "Movies Data"),
+        dashboardSidebar(
+                hr(),
+                sidebarMenu( id = "tabs",
+                        menuItem("Plot1", tabName = "Plot 1", icon = icon("globe-americas")),
+                        menuItem("Plot2", tabName = "Dashboard", icon = icon("dashboard")),
+                        menuItem("Plot3", tabName = "Plot 3", icon = icon("th")),
+                        menuItem("Plot4", tabName = "Plot 4", icon = icon("th")),
+                        menuItem("Plot5", tabName = "Plot 5", icon = icon("th")),
+                hr()
+                        
                 ),
-                
-                mainPanel(
-                        plotlyOutput("barPlot")
+                conditionalPanel("input.tabs == 'Plot 1'", 
+                                 menuItem(
+                                         selectInput("Genre", "Select a Genre:",  
+                                                     choices = c(names(cleanData)), selected = "Total")
+                                 ),
+                                 menuItem(
+                                         selectInput("Continent", "Select a Continent:",
+                                                     choices = c("World" ,"North America", "South America", "Africa", "Europe",
+                                                                 "Asia", "Oceania"), selected = "World")
+                                 ),
+                                 menuItem(
+                                         sliderInput("Number of Countries", "Select the Number of Countries to be shown for bar chart:",
+                                                     min = 3, max = 30, value = 20)
+                                         
+                                 )
+                        )
+        ),
+        dashboardBody(
+                fluidRow(
+                        column( width = 12,
+                                box(
+                                        collapsible = TRUE, title = "Bar Plot of Movie Counts", status = "primary", solidHeader = TRUE,  
+                                        column(width = 12, plotlyOutput("barPlot")),
+                                        width = 12
+                                        
+                                       
+                                ),
+                                
+                                
+                                        
+                                
+                                
+                                box(
+                                        collapsible = TRUE, title = "Map of Movie Counts", status = "primary", solidHeader = TRUE,  
+                                        column(width = 12, plotlyOutput("mapPlot")),
+                                        width = 12
+                                        #plotlyOutput("mapPlot")
+                                        
+                                        
+                                        # plotlyOutput("mapPlot")
+                                        
+                                )
+                        )
+                ),
+                fluidRow(
+                        # box(
+                        #         selectInput("Genre", "Select a Genre:",  
+                        #                     choices = c(names(cleanData)), selected = "Total"),
+                        #         selectInput("Continent", "Select a Continent:",
+                        #                     choices = c("World" ,"North America", "South America", "Africa", "Europe",
+                        #                                 "Asia", "Oceania"), selected = "World"),
+                        #         sliderInput("Number of Countries", "Select the Number of Countries to be shown for bar chart:",
+                        #                     min = 3, max = 30, value = 20),
+                        #         width = 3
+                        # )
                 )
         )
 )
